0689b14931b19b4533edfdfaca57fa6f354a70ef
Julio Cardoso <juliocesarsc2008@gmail.com>
2021-12-01 14:41:38 -0300

    Adicionar arquivos de projeto.

---
+++ Operadores_Condicionais/Operadores_Condicionais/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Operadores_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if, else if, else
+            var a = 1;
+            if (a == 1)
+            {
+                Console.WriteLine("entrou na condição do if");
+            }
+            else if (a == 2)
+            {
+                Console.WriteLine("entrou na condição do else if = 2");
+            }
+            else
+            {
+                Console.WriteLine("entrou na condição do else");
+            }
+
+            //Switch
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Case 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Case 3");
+                    break;
+                default:
+                    Console.WriteLine("Default");
+                    break;
+            }
+
+            //Ternário
+            string texto = (a == 1 ? "A vale 1" : "A tem valor diferente de 1");
+            Console.WriteLine(texto);
+
+        }
+    }
+}
---
+++ Tipos_de_variaveis/Tipos_de_variaveis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tipos_de_variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipagem estática
+            int i = 1;
+            //Tipagem dinâmica
+            var x = 2;
+
+            //Tipagem estática
+            string s = "Nome";
+            //Tipagem dinâmica
+            var s2 = "Nome 2";
+
+            float f = 1.5f;
+            var f2 = 1.5f;
+
+            double d = 1.5;
+            var d2 = 1.5;
+
+            bool bo = true;
+            var bo2 = true;
+
+            char a = 'a';
+            string b = "b";
+
+
+            //conversão
+            bool boolean = bool.Parse("false");          //parse converte string
+            bool boolean2 = Convert.ToBoolean("false");  //Converte objetos para formato boolean
+            d.ToString(); // conversão para string
+        }
+    }
+}
